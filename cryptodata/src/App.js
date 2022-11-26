1f97e066a90c5cdf6267e86c69a519135861d7c8
Prashant Mishra <prashantmishra111@outlook.com>
2022-11-26 16:22:59 +0530

    comments added!!

--- cryptodata/src/App.js
+++ cryptodata/src/App.js
@@ -1,12 +1,15 @@
  import React,{ useState, useEffect } from 'react';
   
  import Table from './Components/Table';
- import './Components/Table.css';
+  
 
 function App() {
 
   const[cryptoData, setCryptoData] = useState([]);
   
+
+  // Fetching the data from the api using useEffect and storing 
+  // it in in cryptoData using setter functions from state.
   useEffect(() =>{
     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
     .then((res) => {
@@ -18,6 +21,7 @@ function App() {
   },[])
 
   return(
+    // passing the crypto data as the prop in table component.
     <Table cryptoData={ cryptoData }/>
   ) 
 }
--- cryptodata/src/Components/Table.js
+++ cryptodata/src/Components/Table.js
@@ -1,7 +1,11 @@
  import './Table.css';
+
+
+
+// receiving the data from parent component as props
 function Table(  {cryptoData}) {
 
-
+ 
   return (
     <div className="Table">
       <div className="Table-header"><h1>Crypto Data</h1></div>
@@ -16,16 +20,23 @@ function Table(  {cryptoData}) {
                 <th>Change Percentage</th>
                 <th>Market Cap</th>
               </tr>
+              {/* using map method to get the access the data */}
           {  cryptoData.map(function(cryptoData, key) {
             return (
+              // each table row has a unique key.
               <tr key={key}>  
                
                   <td id='wrapper'> <span> <img src={cryptoData.image} alt="crypto logo" /> </span> <span> { cryptoData.name }</span></td>  
-                  <td>{ cryptoData.symbol.toUpperCase() } </td>
+                  {/* converting symbol value in uppercase */}
+                  <td>{ cryptoData.symbol.toUpperCase() } </td> 
                   <td>${ cryptoData.current_price  } </td>
                   <td>${ cryptoData.total_volume } </td>
+                  {/* if percentage change is positive it will show in green and if 
+                  it is in negative it will show in red */}
                   {cryptoData.ath_change_percentage > 0 ?   <td style={{color:"green"}}>{ cryptoData.ath_change_percentage }% </td> : <td style={{color:"red"}}>{ cryptoData.ath_change_percentage      }% </td>}
                   <td>Mkt Cap:{ cryptoData.market_cap } </td>
+                  
+
 
                    
               </tr>
